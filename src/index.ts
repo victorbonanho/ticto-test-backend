35a5c0dd0a84dd8e447b48fffb235c0d50b8dcf3
VictorU <victbonanho@gmail.com>
2024-10-15 14:26:39 -0300

    added cors

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import apiRoutesV1 from "./routes/apiRoutesV1";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const mongoUri = process.env.MONGODB_URI;
 
